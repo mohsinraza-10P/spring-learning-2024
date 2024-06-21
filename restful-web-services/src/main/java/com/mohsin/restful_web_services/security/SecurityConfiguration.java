710b93d700b8dc7d528901465b8b3dc3c1ee6f8f
Mohsin Raza <mohsin.raza@10pearls.com>
2024-06-21 19:31:05 +0500

    MySQL variable updates

--- restful-web-services/src/main/java/com/mohsin/restful_web_services/security/SecurityConfiguration.java
+++ restful-web-services/src/main/java/com/mohsin/restful_web_services/security/SecurityConfiguration.java
@@ -13,7 +13,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // 1) All requests should be authenticated
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/").permitAll().anyRequest().authenticated());
         // 2) If a request is not authenticated, a web page is shown
         http.httpBasic(Customizer.withDefaults());
         // 3) CSRF -> POST, PUT
