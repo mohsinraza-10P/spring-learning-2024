fa3a2756ce0732f7b4826a175d437b72aa178212
Mohsin Raza <mohsin.raza@10pearls.com>
2024-07-04 13:09:14 +0500

    JPA postgres project added

---
+++ learn-jpa-postgres/src/main/java/com/mohsin/learn_jpa_postgres/LearnJpaPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.mohsin.learn_jpa_postgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaPostgresApplication.class, args);
+	}
+
+}
